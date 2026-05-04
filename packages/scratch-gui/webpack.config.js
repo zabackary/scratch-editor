dc923f89702beef4f92dc9f11b94a2c7a03577df
zabackary <137591653+zabackary@users.noreply.github.com>
2026-05-04 11:03:07 +0900

    feat: add binaryen dep

--- packages/scratch-gui/webpack.config.js
+++ packages/scratch-gui/webpack.config.js
@@ -59,6 +59,7 @@ const baseConfig = new ScratchWebpackConfigBuilder(
         resourceQuery: /^$/, // reject any query string
         type: 'asset' // let webpack decide on the best type of asset
     })
+    .addExternals(['node:module'])
     .addPlugin(new webpack.DefinePlugin({
         'process.env.DEBUG': Boolean(process.env.DEBUG),
         'process.env.GA_ID': `"${process.env.GA_ID || 'UA-000000-01'}"`,
